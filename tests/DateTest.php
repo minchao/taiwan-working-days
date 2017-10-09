215fc7214917c1b10c869a4346bd34514abc35c4
minchao <minchao.220@gmail.com>
2017-10-10 00:31:03 +0800

    Improve DateTest

--- tests/DateTest.php
+++ tests/DateTest.php
@@ -9,11 +9,19 @@ class DateTest extends TestCase
 {
     public function testConstruct()
     {
-        $target = Date::create('20170101', true, '開國紀念日');
+        $target = Date::create('20170101', true, '');
 
         $this->assertEquals('20170101', $target->format('Ymd'));
         $this->assertEquals(new \DateTimeZone('Asia/Taipei'), $target->getTimezone());
         $this->assertTrue($target->isWorkingDay());
+        $this->assertEmpty($target->getNote());
+
+        $target->setIsWorkingDay(false);
+
+        $this->assertFalse($target->isWorkingDay());
+
+        $target->setNote('開國紀念日');
+
         $this->assertEquals('開國紀念日', $target->getNote());
     }
 }
